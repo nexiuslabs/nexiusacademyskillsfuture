c70a9476bc5f0bc441682406d33d336d0faedf6c
root <root@srv1538713.hstgr.cloud>
2026-07-15 23:54:08 +0000

    match agentic-ai TP logo exactly to advanced page: h-10 sm:h-12

--- components/courses/CourseHero.tsx
+++ components/courses/CourseHero.tsx
@@ -161,7 +161,7 @@ const Hero: React.FC = () => {
                 widths={[128, 256]}
                 sizes="224px"
                 fit="contain"
-                className="w-28 h-auto object-contain"
+                className="h-10 w-auto object-contain sm:h-12"
               />
             </div>
           </div>
